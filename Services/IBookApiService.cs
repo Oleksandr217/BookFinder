0a8cfbbeb3387503ac458bdaa8604226edb1842b
Oleksandr <oleksandrhelpa@gmail.com>
2025-07-14 13:23:05 +0300

    add logic

---
+++ Services/IBookApiService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BookFinder.Model;
+
+namespace BookFinder.Services
+{
+    internal interface IBookApiService
+    {   
+        Task<List<Book>> SearchBooksAsync(string query);
+    }
+}
+    
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using BookFinder.Model;
+using BookFinder.Services;
+using BookFinder.Helpers;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace BookFinder.ViewModel
+{   
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly IBookApiService _apiService;
+
+        public MainViewModel()
+        {
+            _apiService = new GoogleBooksApiService();
+            SearchCommand = new RelayCommand(async _ => await SearchBooks());
+            Books = new ObservableCollection<Book>();
+        }
+
+        private string _searchText; 
+        public string SearchText
+        {
+            get => _searchText;
+            set { _searchText = value; OnPropertyChanged(); }
+        }
+
+        public ObservableCollection<Book> Books { get; set; }
+        public ICommand SearchCommand { get; }
+
+        private async Task SearchBooks()
+        {
+            Books.Clear();
+
+            if (string.IsNullOrWhiteSpace(SearchText))
+            {
+
+                MessageBox.Show("Введіть назву книги для пошуку.", "Порожній запит", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            var result = await _apiService.SearchBooksAsync(SearchText);
+            foreach (var book in result)
+                Books.Add(book);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
+}
